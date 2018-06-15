d1976afc3a2f307dc0614c6b462999b0e0e6146c
kuzminGH <ivan.kuzmin17@gmail.com>
2018-06-16 00:41:40 +0300

    first&second

---
+++ Assets/LevelControler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelControler : MonoBehaviour
+{
+    public static LevelControler current;
+    Vector3 startingPosition;
+
+    void Awake()
+    {
+        current = this;
+    }
+
+    public void setStartPosition(Vector3 pos)
+    {
+        this.startingPosition = pos;
+    }
+    public void onRabitDeath(HeroRabit rabit)
+    {
+        //При смерті кролика повертаємо на початкову позицію
+        rabit.transform.position = this.startingPosition;
+        rabit.transform.rotation = new UnityEngine.Quaternion(0, 0, 0, 0);
+    }
+}
