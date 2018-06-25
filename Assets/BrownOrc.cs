f01493c58e9516e8027d2b1dbdf435492bb822db
kuzminGH <ivan.kuzmin17@gmail.com>
2018-06-25 14:53:35 +0300

    brown orc and carrot

---
+++ Assets/BrownOrc.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrownOrc : Ork
+{
+
+    public GameObject prefabCarrot;
+    public int timeOfThrowing;
+    float timeCur;
+
+
+    // Use this for initialization
+    new void Start()
+    {
+        base.Start();
+        timeCur = timeOfThrowing;
+
+    }
+    //stay when attack
+    protected override float attackDirection()
+    {
+        return 0;
+    }
+
+    protected override void checkAttack()
+    {
+        Vector3 position = this.transform.position;
+        Vector3 rabit_pos = HeroRabit.current.transform.position;
+        if (GetComponent<BoxCollider2D>().IsTouching(HeroRabit.current.GetComponent<BoxCollider2D>()))
+        {
+            if (HeroRabit.current.transform.position.y > transform.position.y)
+            {
+                animator.SetBool("death", true);
+                return;
+            }
+        }
+        if (mode == Mode.Attack)//rabit_pos.x > pointA.x && rabit_pos.x < pointB.x
+        {
+
+
+            if (rabit_pos.x > position.x)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+                if (timeCur > 0)
+                {
+                    timeCur -= Time.deltaTime;
+                    return;
+                }
+                animator.SetTrigger("isAttack");
+                this.launchCarrot(1);
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+                if (timeCur > 0)
+                {
+                    timeCur -= Time.deltaTime;
+                    return;
+                }
+                animator.SetTrigger("isAttack");
+                this.launchCarrot(-1);
+            }
+            timeCur = timeOfThrowing;
+        }
+    }
+
+    void launchCarrot(float direction)
+    {
+        //Створюємо копію Prefab
+        GameObject obj = GameObject.Instantiate(this.prefabCarrot);
+        //Розміщуємо в просторі
+        obj.transform.position = this.transform.position + new Vector3(0, this.GetComponent<BoxCollider2D>().size.y / 2 - 0.2f, 0);
+        //Запускаємо в рух
+        Carrot carrot = obj.GetComponent<Carrot>();
+
+        carrot.launch(direction);
+    }
+
+    protected override bool isDieAnimation()
+    {
+        return animator.GetCurrentAnimatorStateInfo(0).IsName("brownOrcDie");
+    }
+
+
+}
---
+++ Assets/Carrot.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carrot : Collectable
+{
+
+    public float speed = 2.5f;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(destroyLater());
+    }
+
+    public void launch(float direction)
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(direction * speed, 0);
+        if (direction < 0) GetComponent<SpriteRenderer>().flipX = true;
+        else GetComponent<SpriteRenderer>().flipX = false;
+    }
+
+    IEnumerator destroyLater()
+    {
+        yield return new WaitForSeconds(2.0f);
+        Destroy(this.gameObject);
+    }
+
+    protected override void OnRabitHit(HeroRabit rabit)
+    {
+        if (rabit.isBig())
+        {
+            rabit.setBig(false);
+            Vector3 currSize = rabit.transform.localScale;
+            rabit.transform.localScale = new Vector3(currSize.x / 1.5f, currSize.y / 1.5f, 0);
+        }
+        else
+        {
+            rabit.GetComponent<Animator>().SetBool("death", true);
+        }
+        this.CollectedHide();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        HeroRabit rabit = collider.GetComponent<HeroRabit>();
+        if (rabit != null)
+            this.OnRabitHit(rabit);
+        else
+        {
+            BrownOrc orc = collider.GetComponent<BrownOrc>();
+            if (orc== null) this.CollectedHide();
+        }
+    }
+
+}
